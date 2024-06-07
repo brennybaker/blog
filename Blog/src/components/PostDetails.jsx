ee70ae8dc0cc2458b7d06ad73fd15078eff7ba7f
Bre Baker <bakerbr@IT-Q94QY2RQHT.local>
2024-06-07 16:28:57 -0500

    implement delete feature

--- Blog/src/components/PostDetails.jsx
+++ Blog/src/components/PostDetails.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { API_URL } from '../constants';
-
 
 function PostDetails() {
   const [post, setPost] = useState(null);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCurrentPost = async () => {
@@ -26,13 +26,33 @@ function PostDetails() {
     fetchCurrentPost();
   }, [id]);
 
-    if (!post) return <h2>Loading...</h2>;
+  const deletePost = async () => {
+    if (window.confirm("Your post will be deleted if you continue. Still wish to continue?")) {
+      try {
+        const response = await fetch(`${API_URL}/${id}`, {
+          method: 'DELETE',
+        });
+        if (response.ok) {
+          console.log("Post deleted successfully");
+          navigate("/");
+        } else {
+          throw response;
+        }
+      } catch (error) {
+        console.error('An error occurred while deleting the post', error);
+      }
+    }
+  };
+
+  if (!post) return <h2>Loading...</h2>;
 
   return (
     <div>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <Link to="/">Back to Posts List</Link>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <Link to="/">Back to Posts List</Link>
+      {" | "}
+      <button onClick={deletePost}>Delete</button>
     </div>
   );
 }
